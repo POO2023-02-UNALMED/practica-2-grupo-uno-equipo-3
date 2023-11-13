5c76376bc93c954ec0ed3114984341323193f322
Lualmara-5 <luimartinezra@unal.edu.co>
2023-11-13 13:00:55 -0500

    Update Class, Paquete Producto

--- src/gestorAplicacion/productos/paquete.py
+++ src/gestorAplicacion/productos/paquete.py
@@ -1,60 +1,49 @@
-import random
-
-class Producto:
-    contador_productos = 0
-
-    def __init__(self, codigo, peso, volumen=0):
-        self.codigo = codigo
-        self.peso = peso
-        self.volumen = volumen
-        self.costo_del_pedido = 0
-
-        Producto.contador_productos += 1
-
-    def asignar_costo_del_pedido(self):
-        pass
+from productos.producto import Producto
 
 class Paquete(Producto):
     def __init__(self, peso, alto, ancho, largo, fragil, valor_declarado):
-        codigo = Paquete.generar_codigo()
-        volumen = alto * ancho * largo
-        super().__init__(codigo, peso, volumen)
+        codigo = self.generar_codigo()
+        volumen = alto * largo * ancho
+        super().__init__(codigo, volumen, peso)
+        self.valor_declarado = valor_declarado
+        self.peso = peso
         self.alto = alto
         self.ancho = ancho
         self.largo = largo
         self.fragil = fragil
-        self.valor_declarado = valor_declarado
+        self.volumen = alto * largo * ancho
         self.roto = False
 
-    @staticmethod
-    def generar_codigo():
-        return random.randint(10000, 99999)
+    def __str__(self):
+        return "---------------------PRODUCTO--------------------\n" + \
+               "Tipo de producto: Paquete\n" + \
+               "Código de pedido: " + str(self.codigo) + "\n" + \
+               "Peso: " + str(self.peso) + "kg\n" + \
+               "Altura: " + str(self.alto) + "m\n" + \
+               "Ancho: " + str(self.ancho) + "m\n" + \
+               "Largo: " + str(self.largo) + "m\n" + \
+               "Volumen: " + str(self.alto * self.ancho * self.largo) + "m3\n" + \
+               "Fragil: " + ("Sí" if self.fragil else "No") + "\n" + \
+               "Valor declarado: " + str(self.valor_declarado)
 
     def asignar_costo_del_pedido(self):
         tarifa_base_por_kg = 1000
         tarifa_base_por_metro_cubico = 4000
         tarifa_adicional_fragil = 1.25
-
         costo_pedido = (tarifa_base_por_metro_cubico * self.volumen) + (tarifa_base_por_kg * self.peso) + self.valor_declarado * 0.3
-
         if not self.fragil:
             self.costo_del_pedido = costo_pedido
         else:
             self.costo_del_pedido = costo_pedido * tarifa_adicional_fragil
 
-    def __str__(self):
-        return f"---------------------PRODUCTO--------------------\n" \
-               f"Tipo de producto: Paquete\n" \
-               f"Código de pedido: {self.codigo}\n" \
-               f"Peso: {self.peso} kg\n" \
-               f"Altura: {self.alto} m\n" \
-               f"Ancho: {self.ancho} m\n" \
-               f"Largo: {self.largo} m\n" \
-               f"Volumen: {self.volumen} m^3\n" \
-               f"Fragil: {'Sí' if self.fragil else 'No'}\n" \
-               f"Valor declarado: {self.valor_declarado}"
+    def is_fragil(self):
+        return self.fragil
 
-paquete = Paquete(5, 0.5, 0.3, 0.4, True, 100)
-paquete.asignar_costo_del_pedido()
-print(paquete)
-print(f"Costo del pedido: {paquete.costo_del_pedido}")
+    def get_valor_declarado(self):
+        return self.valor_declarado
+
+    def is_roto(self):
+        return self.roto
+
+    def set_roto(self, roto):
+        self.roto = roto
--- src/gestorAplicacion/productos/producto.py
+++ src/gestorAplicacion/productos/producto.py
@@ -1,56 +1,27 @@
 import random
 
 class Producto:
-    contador_productos = 0
-    todos_los_productos = []
+    contador_productos = 0  
 
     def __init__(self, codigo, volumen, peso):
         self.codigo = codigo
         self.peso = peso
         self.volumen = volumen
-        self.costo_del_pedido = 0  # Este no es el costo definitivo del producto
-        self.guia = None
+        self.costo_del_pedido = 0.0  
+        self.guia = None  
 
-        Producto.todos_los_productos.append(self)
         Producto.contador_productos += 1
         self.asignar_costo_del_pedido()
 
-    def __str__(self):
-        return "Código: {}, Peso: {}, Volumen: {}, Costo del Pedido: {}".format(
-            self.codigo, self.peso, self.volumen, self.costo_del_pedido
-        )
-
-    def asignar_costo_del_pedido(self):
-        pass  # Implementa la lógica para asignar el costo del pedido en las clases derivadas
-
     @classmethod
     def generar_codigo(cls):
-        codigo_aleatorio = random.randint(10000, 99999)
-        return codigo_aleatorio
+        return random.randint(10000, 99999)
 
-    # Getters y setters
-    def get_peso(self):
-        return self.peso
+    def asignar_costo_del_pedido(self):
+        pass
 
-    def get_codigo(self):
-        return self.codigo
-
-    def get_volumen(self):
-        return self.volumen
-
-    def get_costo_del_pedido(self):
-        return self.costo_del_pedido
-
-    def get_guia(self):
-        return self.guia
-
-    @classmethod
-    def get_contador_productos(cls):
-        return cls.contador_productos
-
-    @classmethod
-    def get_todos_los_productos(cls):
-        return cls.todos_los_productos
+    def __str__(self):
+        return ""
 
     def set_peso(self, peso):
         self.peso = peso
@@ -64,39 +35,34 @@ class Producto:
     def set_guia(self, guia):
         self.guia = guia
 
+    @property
+    def costo_del_pedido(self):
+        return self.costo_del_pedido
+
+    @property
+    def codigo(self):
+        return self.codigo
+
+    @property
+    def peso(self):
+        return self.peso
+
+    @property
+    def volumen(self):
+        return self.volumen
+
+    @property
+    def guia(self):
+        return self.guia
+
     @classmethod
-    def set_todos_los_productos(cls, todos_los_productos):
-        cls.todos_los_productos = todos_los_productos
+    def contador_productos(cls):
+        return cls.contador_productos
 
-class ProductoAnimal(Producto):
-    def __init__(self, codigo, volumen, peso, nombre_animal, edad_animal, tipo_animal):
-        super().__init__(codigo, volumen, peso)
-        self.nombre_animal = nombre_animal
-        self.edad_animal = edad_animal
-        self.tipo_animal = tipo_animal
+    @classmethod
+    def todos_los_productos(cls):
+        return [] 
 
-    def __str__(self):
-        return super().__str() + ", Nombre del Animal: {}, Edad del Animal: {}, Tipo del Animal: {}".format(
-            self.nombre_animal, self.edad_animal, self.tipo_animal
-        )
-
-    def asignar_costo_del_pedido(self):
-        pass  # Implementa la lógica para asignar el costo del pedido específica para ProductoAnimal
-
-class ProductoProducto(Producto):
-    def __init__(self, codigo, volumen, peso, fragilidad, valor_declarado, altura_paquete, ancho_paquete, largo_paquete, instrucciones_especiales):
-        super().__init__(codigo, volumen, peso)
-        self.fragilidad = fragilidad
-        self.valor_declarado = valor_declarado
-        self.altura_paquete = altura_paquete
-        self.ancho_paquete = ancho_paquete
-        self.largo_paquete = largo_paquete
-        self.instrucciones_especiales = instrucciones_especiales
-
-    def __str__(self):
-        return super().__str() + ", Fragilidad: {}, Valor Declarado: {}, Altura: {}, Ancho: {}, Largo: {}, Instrucciones Especiales: {}".format(
-            self.fragilidad, self.valor_declarado, self.altura_paquete, self.ancho_paquete, self.largo_paquete, self.instrucciones_especiales
-        )
-
-    def asignar_costo_del_pedido(self):
-        pass  # Implementa la lógica para asignar el costo del pedido específica para ProductoProducto
+    @classmethod
+    def set_todos_los_productos(cls, productos):
+        pass
