a5e921e790046b6ee22232f687b4b9a722550c88
kevinramos2 <keramosl@unal.edu.co>
2023-11-15 19:56:08 -0500

    up

--- src/gestorGrafico/recoger.py
+++ src/gestorGrafico/recoger.py
@@ -9,27 +9,68 @@ from excepeciones.ErrorAplicacion import ErrorAplicacion
 from excepeciones.ExcepEntrys import * 
 from excepeciones.ExcepObj import *
 
-def verificarDatos(producto, cedulaDestinatario):
-    guia = producto.getGuia()
-    destinatario = guia.getDestinatario()
-    if int(destinatario.getCedula()) == cedulaDestinatario:
-        return True
-    return False
+class recoger(Frame):
+    def __init__(self,ventana):
+        super().__init__(ventana)
+        self.config(highlightbackground="#085870",highlightthickness=3)
+        self.pack(expand=True)
 
-def encontrarProductoPorCodigo(codigo):
-    for producto in producto.getTodosLosProductos():
-         if producto.getCodigo() == codigo:
-            return producto
-    return None
+        frame = Frame(ventana, width=400, height=200,bg="green",highlightbackground="#085870",highlightthickness=5)
+        frame.pack(fill=tk.BOTH,expand=True)
 
-def cambiar_frame_sucursal(self, event):
-    # Obtener la sucursal seleccionada
-    sucursal_seleccionada = self.combobox_sucursales.get()
+        # Label Titulo
+        self.Label_Titulo = tk.Label(self,text= "Recoger Paquete",font=("Arial",30))
+        self.Label_Titulo.pack(pady=10)
 
-    # Cambiar a otro frame y pasar la sucursal seleccionada
-    frame_sucursal = FrameSucursal(self.master, sucursal_seleccionada)
-    self.pack_forget()  
-    frame_sucursal.pack()
+        self.Label_descripcion = Label(self, "En este apartado podrás reclamar los paquetes que te hayan enviado, solo debes llenar la información que se te pide en los siguientes recuadros", font=("Arial",11),wraplength=250)
+        self.Label_descripcion.pack(pady=10)
+
+        self.Label_escogerSucursal = Label(self,"Escoge la sucursal", font=("Arial",11))
+        self.Label_escogerSucursal.pack(side="left")
+
+        todas_las_sucursales = [s.getNombre() for s in Sucursal.getTodasLasSucursales()]
+        self.combobox_sucursales = ttk.Combobox(self, values=todas_las_sucursales)
+        self.combobox_sucursales.pack(pady=10)
+
+        # Configurar evento para cambio en el ComboBox
+        self.combobox_sucursales.bind("<<ComboboxSelected>>", self.cambiar_frame_sucursal)
+
+
+        # Limpiar el contenido actual del widget Text
+        self.texto_tabla.delete(1.0, tk.END)
+
+
+        Nombre_dest = Label(frame,text="Ingrese su nombre:", font=("Arial",11))
+        entrada_Nombre = Entry(frame)
+        entrada_Nombre.pack(side="bottom", pady=5)
+        Cedula_dest = Label(frame, text="Ingrese su cédula:",font=("Arial",11))
+        entrada_cedula = Entry(frame)
+        entrada_cedula.pack(side="bottom",pady=10)
+
+
+    def verificarDatos(producto, cedulaDestinatario):
+            guia = producto.getGuia()
+            destinatario = guia.getDestinatario()
+            if int(destinatario.getCedula()) == cedulaDestinatario:
+                return True
+            return False 
+
+
+    def encontrarProductoPorCodigo(codigo):
+            for producto in producto.getTodosLosProductos():
+                if producto.getCodigo() == codigo:
+                    return producto
+            return None
+
+
+    def cambiar_frame_sucursal(self, event):
+        # Obtener la sucursal seleccionada
+        sucursal_seleccionada = self.combobox_sucursales.get()
+
+        # Cambiar a otro frame y pasar la sucursal seleccionada
+        frame_sucursal = FrameSucursal(self.master, sucursal_seleccionada)
+        self.pack_forget()  
+        frame_sucursal.pack()
 
 
 class FrameSucursal(tk.Frame):
@@ -39,15 +80,16 @@ class FrameSucursal(tk.Frame):
         self.config(bg="#085870")
         self.pack(fill="both",expand=True)
 
+         # Crear contenido para el nuevo frame
+        etiqueta = tk.Label(self, text=f"Ha seleccionado la sucursal: {self.sucursal_seleccionada}",font=("Arial",20))
+        etiqueta.pack(pady=10)
+
 class ReclamarPaquete(tk.Frame):
     def __init__(self, ventana):
         super().__init__(ventana)
         self.config(highlightbackground="#085870", highlightthickness=3)
 
-        # Label Titulo
-        self.Label_Titulo = tk.Label(self,text= "Recoger Paquete",font=("Arial",30))
-        self.Label_Titulo.pack(pady=10)
-
+        
         self.Label_Descripcion = tk.Label(self, text="En este apartado podrás reclamar los paquetes que te hayan enviado, solo debes llenar la información que se te pide en los siguientes recuadros",font=("Arial",10),wraplength=250)
         self.Label_Descripccion.pack(pady=10)
 
@@ -59,11 +101,6 @@ class ReclamarPaquete(tk.Frame):
         # Configurar evento para cambio en el ComboBox
         self.combobox_sucursales.bind("<<ComboboxSelected>>", self.cambiar_frame_sucursal)
 
-
-        def limpiar1(self, ventana):
-            nombre_Destinatario2.delete(0, last= END)
-            codigoPaqueteE.delete(0, last= END)
-            cedula_Destinatario2.delete(0, last= END)
 
 
             
@@ -131,5 +168,4 @@ class ReclamarPaquete(tk.Frame):
                                bg="#085870")
         botonReclamar.grid(row=4, column=0, padx=10, pady=10, sticky='e')
 
-        botonLimpiar = Button(sFrame, text="Limpiar", command=limpiar1, font=("Arial", 11), fg="white", bg="#085870")
-        botonLimpiar.grid(row=4, column=1, padx=10, pady=10)
+
